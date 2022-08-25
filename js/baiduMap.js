716d5753e805bd03ea2de86f670d57a0fa05d9f7
D1isney <2497050447@qq.com>
2022-08-25 11:20:55 +0800

    commit

---
+++ js/baiduMap.js
@@ -0,0 +1,1 @@
+(function(){window.BMAP_PROTOCOL = "https";window.BMap_loadScriptTime = (new Date).getTime();document.write('<script type="text/javascript" src="https://api.map.baidu.com/getscript?v=3.0&ak=d7ZTrW4uIPhoFq1c6oTRFkhKOa3iVGhP&services=&t=20220720180520"></script>');})();
